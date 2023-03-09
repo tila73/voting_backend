900674ade556400d2fb17a4a71253ce63279f7b3
Tila <tila.ale00@gmail.com>
2023-03-09 15:51:35 +0545

    models added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    companyImg = models.ImageField(upload_to='img/', blank=False, null=False)
+    title = models.CharField(max_length=150)
+    desc = models.TextField()
+    mission = models.TextField()
+    vision = models.TextField()
+    status = models.BooleanField(default=True, help_text="0=inactive, 1=active")
+
+    def __str__(self):
+        return self.title
+    
+class Service(models.Model):
+    servicesImg = models.ImageField(upload_to='img/services', blank=False, null=False)
+    title = models.CharField(max_length=150)
+    desc = models.TextField()
+    status = models.BooleanField(default=True, help_text="0=inactive, 1=active")
+
+    def __str__(self):
+        return self.title
+    
+class WhyChooseUs(models.Model):
+    title = models.CharField(max_length=150)
+    img = models.ImageField(upload_to='img/', blank=False, null=False)
+    desc = models.TextField()
+    status = models.BooleanField(default=True, help_text="0=inactive, 1=active")
+
+    def __str__(self):
+        return self.title
+    
+class Teams(models.Model):
+    title = models.CharField(max_length=150)
+    sub_desc = models.TextField()
+    img = models.ImageField(upload_to='img/teams', blank=False, null=False)
+    name = models.CharField(max_length=255)
+    post = models.CharField(max_length=255)
+    fb_link = models.CharField(max_length=255)
+    twitter_link = models.CharField(max_length=255)
+    insta_link = models.CharField(max_length=255)
+    status = models.BooleanField(default=True, help_text="0=inactive, 1=active")
+
+    def __str__(self):
+        return self.title
