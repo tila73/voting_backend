9d4b9b9486a207952bace735f07d86e762a8c6fa
nikitaadh <nikitaadhi8@gmail.com>
2023-03-12 14:38:16 +0545

    views and urls for templates added

--- main/views.py
+++ main/views.py
@@ -5,5 +5,41 @@ from django.shortcuts import render
 def home(request):
     return render(request, "home/index.html")
 
+def voting(request):
+    return render(request, "home/voting.html")
+
+def about(request):
+    return render(request, "home/about.html")
+
+def company(request):
+    return render(request, "home/company.html")
+
+def team(request):
+    return render(request, "home/team.html")
+
+def testimonial(request):
+    return render(request, "home/testimonial.html")
+
 def event(request):
     return render (request,'home/event.html')
+
+def blog(request):
+    return render(request, "home/blog.html")
+
+def gallery(request):
+    return render(request, "home/gallery.html")
+
+def news(request):
+    return render(request, "home/news.html")
+
+def faq(request):
+    return render(request, "home/faq.html")
+
+def contact(request):
+    return render(request, "home/contact.html")
+
+def login(request):
+    return render(request, "home/login.html")
+
+def register(request):
+    return render(request, "home/register.html")
