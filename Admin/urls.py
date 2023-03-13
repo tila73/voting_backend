1188b95b89fe8437ef2cb79814ddb5a1c11c7d04
PopsicleG007 <prabeshgiri08@gmail.com>
2023-03-13 12:25:05 +0545

    prabesh-update

--- Admin/urls.py
+++ Admin/urls.py
@@ -5,6 +5,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.Dashboard, name="Dashboard"),  
     path('Event', views.Event, name="Event"),  
+    path('faq/', views.faq, name="faq"),
+    path('teams/', views.teams, name="teams"),
+    path('services/', views.services, name="services"),
+    path('whychooseus/', views.whychooseus, name="whychooseus"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Admin/views.py
+++ Admin/views.py
@@ -7,3 +7,15 @@ def Dashboard(request):
 
 def Event(request):
     return render(request, 'admin/event.html')
+
+def faq(request):
+    return render(request, 'admin/faq.html')
+
+def teams(request):
+    return render(request, 'admin/teams.html')
+
+def services(request):
+    return render(request, 'admin/services.html')
+
+def whychooseus(request):
+    return render(request, 'admin/whychooseus.html')
